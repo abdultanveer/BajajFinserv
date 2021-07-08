f912a730a6916e22ad403a705aedccc603c2eeaf
abdul <abdul.tanveer@gmail.com>
2021-07-08 15:52:42 +0530

    button click-- getting text from edittext

--- app/src/main/java/com/abdul/bajajfinserv/MainActivity.kt
+++ app/src/main/java/com/abdul/bajajfinserv/MainActivity.kt
@@ -3,15 +3,19 @@ package com.abdul.bajajfinserv
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {  //why the word compat,
+     lateinit var nameEditText: EditText   //declaration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nameEditText = findViewById(R.id.editTextTextPersonName) //instantiation, initialization
     }
 
     fun clickHandler(view: View) {
-        Toast.makeText(this,"welcome to android",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,nameEditText.text,Toast.LENGTH_SHORT).show()
     }
 }
