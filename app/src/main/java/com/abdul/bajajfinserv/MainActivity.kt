0338e6a2b4e54b84ab620f5ba6973660fe0b0abf
abdul <abdul.tanveer@gmail.com>
2021-07-08 15:57:44 +0530

    button click-- getting text from edittext -- setting it on textview

--- app/src/main/java/com/abdul/bajajfinserv/MainActivity.kt
+++ app/src/main/java/com/abdul/bajajfinserv/MainActivity.kt
@@ -4,18 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {  //why the word compat,
      lateinit var nameEditText: EditText   //declaration
+     lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         nameEditText = findViewById(R.id.editTextTextPersonName) //instantiation, initialization
+        textView = findViewById(R.id.resTextview)
     }
 
     fun clickHandler(view: View) {
+        textView.text = nameEditText.text
         Toast.makeText(this,nameEditText.text,Toast.LENGTH_SHORT).show()
     }
 }
