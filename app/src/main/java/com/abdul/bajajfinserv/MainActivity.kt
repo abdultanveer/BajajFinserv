9f176d4f3eeb16b8397d3e60dcfe338e62792a70
abdul <abdul.tanveer@gmail.com>
2021-07-08 15:36:27 +0530

    button click

--- app/src/main/java/com/abdul/bajajfinserv/MainActivity.kt
+++ app/src/main/java/com/abdul/bajajfinserv/MainActivity.kt
@@ -2,10 +2,16 @@ package com.abdul.bajajfinserv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {  //why the word compat,
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickHandler(view: View) {
+        Toast.makeText(this,"welcome to android",Toast.LENGTH_SHORT).show()
     }
 }
