7a18afee57245fa225d6dbe36ebeaf75703a3773
abdul <abdul.tanveer@gmail.com>
2021-07-09 15:47:42 +0530

    logs and logcat

--- app/src/main/java/com/abdul/bajajfinserv/MainActivity.kt
+++ app/src/main/java/com/abdul/bajajfinserv/MainActivity.kt
@@ -3,30 +3,51 @@ package com.abdul.bajajfinserv
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {  //why the word compat,
+    var TAG = MainActivity::class.simpleName
      lateinit var nameEditText: EditText   //declaration
      lateinit var textView: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)//show/inflate activity_main file
         nameEditText = findViewById(R.id.editTextTextPersonName) //instantiation, initialization
         textView = findViewById(R.id.resTextview)
+        Log.i(TAG,"onCreate")
+
     }
 
     fun clickHandler(view: View) {
-        textView.text = nameEditText.text
+        Log.v(TAG,"clickHandler")
+
+        createAlarm("wake up",3,18)
+       /* textView.text = nameEditText.text
         Toast.makeText(this,nameEditText.text,Toast.LENGTH_SHORT).show()
         var homeIntent: Intent;
         homeIntent = Intent(this,HomeActivity::class.java).apply {
-            putExtra("mkey", nameEditText.text)
+            putExtra("mkey", nameEditText.text.toString())
         }
-        startActivity(homeIntent)
+        startActivity(homeIntent)*/
 
+    }
+
+
+    fun createAlarm(message: String, hour: Int, minutes: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_HOUR, hour)
+            putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 }
