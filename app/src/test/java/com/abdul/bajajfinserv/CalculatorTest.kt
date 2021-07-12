c34294103f6ffbd7f854427b0c105dd16092daa8
abdul <abdul.tanveer@gmail.com>
2021-07-12 11:48:26 +0530

    unit testing

---
+++ app/src/test/java/com/abdul/bajajfinserv/CalculatorTest.kt
@@ -0,0 +1,16 @@
+package com.abdul.bajajfinserv
+
+import junit.framework.TestCase
+
+class CalculatorTest : TestCase(){
+
+   fun test_Add()
+    {
+        var calculator = Calculator();
+        var expected = 40
+        var actual = calculator.add(10, 20)
+        assertEquals(expected,actual)
+    }
+
+
+}
