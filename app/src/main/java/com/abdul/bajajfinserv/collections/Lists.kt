922709753265a477b5a10b36775e12fc67b915f2
abdul <abdul.tanveer@gmail.com>
2021-07-12 15:04:37 +0530

    lists in kotlin

---
+++ app/src/main/java/com/abdul/bajajfinserv/collections/Lists.kt
@@ -0,0 +1,20 @@
+package com.abdul.bajajfinserv.collections
+
+class Lists {
+
+}
+
+fun main() {
+
+
+    val numbers: List<Int> = listOf(1,2,3,4,5,6)
+    println("List: $numbers")
+    println("Size: ${numbers.size}")
+    println("First element is: ${numbers[0]}")
+    println("Second element is: ${numbers[1]}")
+    println("last  element is: ${numbers[numbers.size-1]}")
+    println("Last index is: ${numbers.size-1}")
+
+
+
+}
