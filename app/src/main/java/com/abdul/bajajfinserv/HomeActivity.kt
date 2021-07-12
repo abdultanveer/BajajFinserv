062e9f3726f7116c2acff60f8563a90039f3926c
abdul <abdul.tanveer@gmail.com>
2021-07-12 11:04:52 +0530

    lifecycle of an activity

--- app/src/main/java/com/abdul/bajajfinserv/HomeActivity.kt
+++ app/src/main/java/com/abdul/bajajfinserv/HomeActivity.kt
@@ -3,15 +3,19 @@ package com.abdul.bajajfinserv
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     lateinit var etHome: EditText
+
+    var TAG = HomeActivity::class.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        Log.i(TAG,"oncreate")
         etHome = findViewById<EditText>(R.id.etHome)
 
       /* var name =  intent.getStringExtra("mkey")
@@ -19,6 +23,30 @@ class HomeActivity : AppCompatActivity() {
             text = name
         }*/
        // hTextView.text = name
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG,"onstart")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG,"onresume -- restore the game state")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG,"onpause --- store the game state")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG,"onstop")
 
     }
 
