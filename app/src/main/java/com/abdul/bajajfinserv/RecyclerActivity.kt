cd8f6bfea27e6e1bcb91b460c9a33145a1adef60
abdul <abdul.tanveer@gmail.com>
2021-07-13 10:21:26 +0530

    setting layout manager and adapter on recyclerview

--- app/src/main/java/com/abdul/bajajfinserv/RecyclerActivity.kt
+++ app/src/main/java/com/abdul/bajajfinserv/RecyclerActivity.kt
@@ -2,7 +2,9 @@ package com.abdul.bajajfinserv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.abdul.bajajfinserv.adapter.ItemAdapter
 import com.abdul.bajajfinserv.data.Datasource
@@ -11,12 +13,13 @@ class RecyclerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler)
-        val textView: TextView = findViewById(R.id.textViewRA)
-        textView.text = Datasource().loadAffirmations().size.toString()
+        /*val textView: TextView = findViewById(R.id.textViewRA)
+        textView.text = Datasource().loadAffirmations().size.toString()*/
 
         val myDataset = Datasource().loadAffirmations()
-        val recyclerView = findViewById<RecyclerView>(findViewById(R.id.recyclerView))
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.adapter = ItemAdapter(this,myDataset)
         recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
